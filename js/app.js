5b097653a50a564369e6875a4f82817e84b0112c
stevenshzf <sanchezsteven.ss@gmail.com>
2021-04-22 16:13:49 -0500

    Modificación de la ruta del serviceWorker

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var url = window.location.href;
 var swLocation = 'twittor/sw.js';
 
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    // navigator.serviceWorker.register('/sw.js');
 
     if (url.includes('localhost')) {
         swLocation = '/sw.js';
